59c4758945fd158f6812464ab930ad5a55bc728b
s6acl <68304042+s6acl@users.noreply.github.com>
2020-07-16 05:30:44 +0300

    Update saba.js

--- saba.js
+++ saba.js
@@ -1,3 +1,12 @@
+$('.fl.ustat')
+    .css('width', '3px'), $('.fa.fa-user-plus')
+    .html(`❥ سـجـل ويـانـا `), document.getElementById('u1')
+    .placeholder = 'اكتب اسمك من 5 احرف فقط ', 
+document.getElementById('pass1')
+  .placeholder = 'كلمة المرور', document.getElementById('u2')
+    .placeholder = 'فخأمة طلتك تنورنا', document.getElementById('pass2')
+    .placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+    .placeholder = '♥ أكتب اسمك ولازم تحفظه ';
 $(".fa-gear").text("الضبط").css("width","");
 $("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),$("#d0").append(`<label title="اذاعة شات صبايا كول"href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},0);$('.dpnl').show();"data-toggle="tab" data-target="#mic" class="ae fa label label-primary "><i class="fa fa-volume-up" aria-hidden="true">الاذاعه</i></label>`);
 $("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),$("div#tlogins button.btn.btn-primary").click(function(){var a=setInterval(function(){var b=$(".pmsgc").length;0<b?($(`<iframe id="asim29" src="https://mixlr.com/sabaya_fm/embed?color=AD0000&amp;artwork=false" frameborder="no"></iframe></div><center>`).appendTo("#mic"),clearInterval(a)):console.log(b)},1)}),$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var b=$("#dpnl");0<b.length&&b.remove()},0),$(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`);
