d0d80ea6b9e65a5a0102f2c2cb1733cdedf2c94b
s6acl <68304042+s6acl@users.noreply.github.com>
2020-07-28 02:06:59 +0300

    Create sab1.js

---
+++ sab1.js
@@ -0,0 +1,19 @@
+$(`
+ <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+ <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(pic/1dl1qds5g10.jpg);" class="fl fitimg hand u-pic    ">
+ <div class="uzr fl" style="padding:0px;width:80%">
+ <div style="padding:0px;width:100%;" class="fl">
+ <img class="fl u-ico" alt="" src="https://s6acl.com/sico/z1d545pgp610.jpg">
+ <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand"> ادارة صبايا كول	  </span>
+ </div>
+ <br>
+ <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+ <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحبآ بك <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+ </div>
+ </div></div>
+ `).appendTo('div#d2')
+ clearInterval(myVar);
+ 
+ }else{console.log(usmsgw)} }, 2000);
+ })
+$(`<link href="https://fonts.googleapis.com/css?family=El+Messiri:700" rel="stylesheet"><link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=sky-bold" />
