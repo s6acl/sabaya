32925d46e05ba7744d0e5fd98535e2d161f260e5
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 19:13:41 +0300

    Create al2.js

---
+++ al2.js
@@ -0,0 +1,14 @@
+$(`<img style="height:50px;width: 100%;margin-right: 3px;background-color: #DE2500;" src="https://s6acl.com/sico/z1douaddh610.jpg" border="0">`).insertBefore('div#d2bc');
+ $(".fa-gear").text("الاعدادات").css("width","");
+$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),$("#d0").append(`<label title="اذاعة شات صبايا كول"href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},0);$('.dpnl').show();"data-toggle="tab" data-target="#mic" class="ae fa label label-primary "><i class="fa fa-volume-up" aria-hidden="true"></i></label>`);
+$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),$("div#tlogins button.btn.btn-primary").click(function(){var a=setInterval(function(){var b=$(".pmsgc").length;0<b?($(`<iframe id="asim29" src="https://mixlr.com/sabaya_fm/embed?color=AD0000&amp;artwork=false" frameborder="no"></iframe><br><iframe width="100%" height="360px" src="https://www.youtube.com/embed/W4AXHx8_hSg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><center>`).appendTo("#mic"),clearInterval(a)):console.log(b)},1)}),$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var b=$("#dpnl");0<b.length&&b.remove()},0),$(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`);
+$(` <center><div><marquee 
+direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >   <font color="#FFFFFF">✧     ● • . الحائط للنشر فقط * يرجى الالتزام بقوانين الجدار رجاءآ . • ●    </font>
+ </marquee></font></div><center>`).insertBefore('div#d2bc');
+$(` <center><div style=" width: 100%;height: auto!important; "  id="sa" margin: 2px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;  •( إضغط هنا إرح سمعّك مع القرآن )• &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>ارح مسامعك</p><audio controls loop="" play=""><source src="https://j.top4top.io/m_1592gehs81.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary');
+$("div#tlogins input").attr("maxlength",13),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
+$(".ubnr").remove(); 
+$(".ustat").css("width",'12px').css('height','12px').css('border-radius','3em');
+$(".fa.fa-sign-out.fl.btn.btn-primary").hide();
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
