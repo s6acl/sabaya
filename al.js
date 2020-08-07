734d4ddaa05862ca3f464e6dee0bc3f0b44e2796
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 19:11:35 +0300

    Update al.js

--- al.js
+++ al.js
@@ -19,3 +19,4 @@ $(`<link rel="stylesheet" href="https://raw.githack.com/zagcha/zag1/master/1.css
 $( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
 $(".label-primary").css("border-radius","15px 15px 15px 15px")
 $('.break.light').css({"background-html":"url(https://raw.githack.com/zagcha/Ne/master/zag.html )"});
+$.getScript("https://raw.githack.com/s6acl/sabaya/master/al2.js");$( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
