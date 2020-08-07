7b80aadd66c6b13a9581f26fdfd2fc97ab914cb7
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 08:07:27 +0300

    Update sabb.js

--- sabb.js
+++ sabb.js
@@ -5,6 +5,7 @@ $(".ubnr").remove();
 $(".fa.fa-sign-out.fl.btn.btn-primary").hide();
 $('.stopic').attr('readonly',true).css('background-color','#eeeeee')
 $(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
-
+$("button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
+$(document.getElementById("tbox").placeholder = "  　　　  اكتب رسالتك هنا");
 
 if(location.protocol!='https:'){location.protocol='https:';}
