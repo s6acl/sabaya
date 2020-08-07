f624b633f5d664063a4315293c0e208c1f9cbae3
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 18:53:54 +0300

    Update sab.js

--- sab.js
+++ sab.js
@@ -6,3 +6,24 @@ $(` <center><div><marquee
 direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >   <font color="#FFFFFF">✧     ● • . الحائط للنشر فقط * يرجى الالتزام بقوانين الجدار رجاءآ . • ●    </font>
  </marquee></font></div><center>`).insertBefore('div#d2bc');
 $(` <center><div style=" width: 100%;height: auto!important; "  id="sa" margin: 2px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;  •( إضغط هنا إرح سمعّك مع القرآن )• &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>ارح مسامعك</p><audio controls loop="" play=""><source src="https://j.top4top.io/m_1592gehs81.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary');
+$("div#tlogins input").attr("maxlength",13),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
+$(`<
+<a id="des3" href="img.html" style="transform:matrix(1, 0, 0, 1, 150, 112);border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-image btn btn-primary"></a>
+  <a id="des3" href="#" style="border: 1px solid white;transform:matrix(1, 0, 0, 1, 112, 112);border-radius: 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-paint-brush btn btn-primary"></a>
+   <a id="des3" href="esh.html" style="transform:matrix(1, 0, 0, 1, 74, 112);border: 1px solid white;border-radius: 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-star btn btn-primary"></a>
+ <a id="des3" href="tell.html" style="transform:matrix(1, 0, 0, 1, 36, 43);border: 1px solid white;border-radius: 0px!important;width:10%;margin:-69px 2px 2px;" class="fl mini fa fa-phone btn btn-primary"></a>
+ <a id="des3" href="z5r.html" style="transform:matrix(1, 0, 0, 1, -2, 8);border: 1px solid white;border-radius: 15px 0px 0px 0px!important;width:10%;margin:-34px 2px 2px;" class="fl mini  fa   fa fa-heartbeat btn btn-primary"></a>
+<a id="des3" style="color: #ffffff;text-align: center;height: 22px;border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:130px;float:right;margin:-68px 2px 2px;transform:matrix(1, 0, 0, 1, 5, -133);background-color: #aa29a9!important;
+ `).insertBefore('.nav-tabs');
+$(".ubnr").remove(); 
+$(".ustat").css("width",'12px').css('height','12px').css('border-radius','3em');
+$(".fa.fa-sign-out.fl.btn.btn-primary").hide();
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
+$("button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
+$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$(`<link rel="stylesheet" href="https://raw.githack.com/zagcha/zag1/master/1.css"><style>`).insertBefore("body");
+
+if(location.protocol!='https:'){location.protocol='https:';}
