e5b94bd3ccc629eab093e45dee7e564cec655742
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 05:42:24 +0300

    Create sabb.js

---
+++ sabb.js
@@ -0,0 +1,10 @@
+$("div#tlogins input").attr("maxlength",13),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
+
+$(".ubnr").remove(); 
+
+$(".fa.fa-sign-out.fl.btn.btn-primary").hide();
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
+
+
+if(location.protocol!='https:'){location.protocol='https:';}
