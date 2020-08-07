4ebee22d2e7944a9e782bf1779bdab37e54abff5
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 19:09:32 +0300

    Create al.js

---
+++ al.js
@@ -0,0 +1,21 @@
+$(`<img src="https://c.top4top.io/p_1680kiupw1.jpg" class="fr " style="Height:25%;width:100%;margin-right: 2px;">
+`).insertBefore('.nav-tabs');
+$(`
+
+</style>
+<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="https://raw.githack.com/zagcha/d3/master/zag1.css">
+<link rel="stylesheet" href="https://cdn.statically.io/gh/ita7a/ita7a.com1/4d477d29/ita7a.css">
+<link rel="stylesheet" href="https://raw.githack.com/Zagchatt/new/master/zag.css">
+<link rel="stylesheet" href="https://raw.githack.com/zagcha/v1/master/new1.css">
+<link rel="https://raw.githack.com/zagcha/newj/master/zag.css">
+</style>`).insertBefore('body');
+$(`<div style=" color: #006E69;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', 
+Fontawesome">الاعلانات للضرورة فقط</div>`).insertBefore('span.fl.fa.fa-send');
+$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$(`<link rel="stylesheet" href="https://raw.githack.com/zagcha/zag1/master/1.css"><style>`).insertBefore("body");
+$( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
+$(".label-primary").css("border-radius","15px 15px 15px 15px")
+$('.break.light').css({"background-html":"url(https://raw.githack.com/zagcha/Ne/master/zag.html )"});
