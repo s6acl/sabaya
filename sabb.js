fec611cf63051abe18fc4ca72e7f8219d0b85cf9
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 08:31:37 +0300

    Update sabb.js

--- sabb.js
+++ sabb.js
@@ -1,7 +1,7 @@
 $("div#tlogins input").attr("maxlength",13),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
 
 $(".ubnr").remove(); 
-
+$(".ustat").css("width",'12px').css('height','12px').css('border-radius','3em');
 $(".fa.fa-sign-out.fl.btn.btn-primary").hide();
 $('.stopic').attr('readonly',true).css('background-color','#eeeeee')
 $(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
