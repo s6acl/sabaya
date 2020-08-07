a63d5f069b0b296338fde1b4162a9345390cfdea
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 17:55:07 +0300

    Update sabb.js

--- sabb.js
+++ sabb.js
@@ -1,5 +1,12 @@
 $("div#tlogins input").attr("maxlength",13),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
-
+$(`<img src="https://c.top4top.io/p_1680kiupw1.jpg" class="fr" style="width:100%;margin-top: 0px;">
+<a id="des3" href="img.html" style="transform:matrix(1, 0, 0, 1, 150, 112);border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-image btn btn-primary"></a>
+  <a id="des3" href="#" style="border: 1px solid white;transform:matrix(1, 0, 0, 1, 112, 112);border-radius: 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-paint-brush btn btn-primary"></a>
+   <a id="des3" href="esh.html" style="transform:matrix(1, 0, 0, 1, 74, 112);border: 1px solid white;border-radius: 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-star btn btn-primary"></a>
+ <a id="des3" href="tell.html" style="transform:matrix(1, 0, 0, 1, 36, 43);border: 1px solid white;border-radius: 0px!important;width:10%;margin:-69px 2px 2px;" class="fl mini fa fa-phone btn btn-primary"></a>
+ <a id="des3" href="z5r.html" style="transform:matrix(1, 0, 0, 1, -2, 8);border: 1px solid white;border-radius: 15px 0px 0px 0px!important;width:10%;margin:-34px 2px 2px;" class="fl mini  fa   fa fa-heartbeat btn btn-primary"></a>
+<a id="des3" style="color: #ffffff;text-align: center;height: 22px;border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:130px;float:right;margin:-68px 2px 2px;transform:matrix(1, 0, 0, 1, 5, -133);background-color: #aa29a9!important;
+ `).insertBefore('.nav-tabs');
 $(".ubnr").remove(); 
 $(".ustat").css("width",'12px').css('height','12px').css('border-radius','3em');
 $(".fa.fa-sign-out.fl.btn.btn-primary").hide();
