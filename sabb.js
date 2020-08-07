4e9134fd9182ffbbc42a55a64a4357ad70df1f33
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 18:35:29 +0300

    Update sabb.js

--- sabb.js
+++ sabb.js
@@ -1,5 +1,5 @@
 $("div#tlogins input").attr("maxlength",13),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
-$(`<img src="https://c.top4top.io/p_1680kiupw1.jpg" class="fr" style="width:100%;margin-top: 0px;">
+$(`<
 <a id="des3" href="img.html" style="transform:matrix(1, 0, 0, 1, 150, 112);border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-image btn btn-primary"></a>
   <a id="des3" href="#" style="border: 1px solid white;transform:matrix(1, 0, 0, 1, 112, 112);border-radius: 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-paint-brush btn btn-primary"></a>
    <a id="des3" href="esh.html" style="transform:matrix(1, 0, 0, 1, 74, 112);border: 1px solid white;border-radius: 0px!important;width:10%;margin:-138px 2px 2px;" class="fl mini fa fa-star btn btn-primary"></a>
@@ -13,6 +13,5 @@ $(".fa.fa-sign-out.fl.btn.btn-primary").hide();
 $('.stopic').attr('readonly',true).css('background-color','#eeeeee')
 $(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');
 $("button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
-$(document.getElementById("tbox").placeholder = "  　　　  اكتب رسالتك هنا");
 
 if(location.protocol!='https:'){location.protocol='https:';}
