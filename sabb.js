3219615602d887a4ce5904a0dca07ddf993ab6c0
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-07 17:33:12 +0300

    Update sabb.js

--- sabb.js
+++ sabb.js
@@ -9,3 +9,11 @@ $("button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
 $(document.getElementById("tbox").placeholder = "  　　　  اكتب رسالتك هنا");
 
 if(location.protocol!='https:'){location.protocol='https:';}
+</style>
+<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="https://raw.githack.com/zagcha/d3/master/zag1.css">
+<link rel="stylesheet" href="https://cdn.statically.io/gh/ita7a/ita7a.com1/4d477d29/ita7a.css">
+<link rel="stylesheet" href="https://raw.githack.com/Zagchatt/new/master/zag.css">
+<link rel="stylesheet" href="https://raw.githack.com/zagcha/v1/master/new1.css">
+<link rel="https://raw.githack.com/zagcha/newj/master/zag.css">
+</style>`).insertBefore('body');
