e0459ed31457f8c37ceaeecde915ccb119e193f8
s6acl <68304042+s6acl@users.noreply.github.com>
2020-08-17 10:32:01 +0300

    Create al3.js

---
+++ al3.js
@@ -0,0 +1,12 @@
+$(".ustat").css("width",'12px').css('height','12px').css('border-radius','3em');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border-radius: 75px 75px 75px 75px;}
+.fr.co {
+    transform: rotate(0deg);
+    height: 10px;
+    width: 16px!important;
+    margin-top: 4px;
+}
+</style>`).insertBefore('body');
